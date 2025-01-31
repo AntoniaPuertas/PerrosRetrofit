0d68fc5d6f1a998a60fc17b050a7b8f2811e34d0
Totilla <tsm.antonia@gmail.com>
2025-01-31 08:30:58 +0100

    Interceptor implementado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.6")
-
+//    Para interceptar las llamadas y respuestas https
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
 
     testImplementation(libs.junit)
--- app/src/main/java/com/antonia/perrosdelmundo/Network/ApiService.kt
+++ app/src/main/java/com/antonia/perrosdelmundo/Network/ApiService.kt
@@ -1,7 +1,9 @@
 package com.antonia.perrosdelmundo.Network
 
 import com.antonia.perrosdelmundo.Data.DogsResponse
-import retrofit2.Response
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -13,13 +15,29 @@ interface APIService {
     @GET("hound/images")
     suspend fun getDogsByBreeds(): DogsResponse
 
+
     companion object{
+        // 2. Crea el interceptor y configura el nivel de logging
+        val logging = HttpLoggingInterceptor().apply {
+            level = Level.BODY  // Muestra el cuerpo completo de peticiones y respuestas
+            // Otros niveles disponibles:
+            // Level.BASIC    - Solo logs de peticiones y respuestas
+            // Level.HEADERS  - Logs de cabeceras
+            // Level.NONE     - Sin logs
+        }
+
+        // 3. Configura el cliente OkHttp con el interceptor
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
         private var apiService: APIService? = null
         private var url:String = "https://dog.ceo/api/breed/"
         fun getInstance():APIService{
             if(apiService == null){
                 apiService = Retrofit.Builder()
                     .baseUrl(url)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
                     .create(APIService::class.java)
